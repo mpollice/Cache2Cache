f63bf0e507b70a051251527688a98b0508ed264d
Marcus Pollice <marcus.pollice@gmail.com>
2017-08-11 12:12:03 +0200

    applied autoformatting to prettify whitespace, minor whitespace improvements

--- Cache2Cache.cpp
+++ Cache2Cache.cpp
@@ -31,109 +31,111 @@ SOFTWARE.
 
 
 struct pingPongPtrs_t {
-  volatile int *pPing;
-  volatile int *pPong;
+	volatile int *pPing;
+	volatile int *pPong;
 };
+
 const int NITER = 100000000;
+
 DWORD WINAPI PingPongThreadProc(void *pingPongPtrs)
 {
-  volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
-  volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
+	volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
+	volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
 
-  *pPong = 0;
-  for (int i = 0; i < NITER; ++i)
-  {
-      while (*pPing < i);
-      *pPong = i+1;
-  }
-  return 0;
+	*pPong = 0;
+	for (int i = 0; i < NITER; ++i)
+	{
+		while (*pPing < i);
+		*pPong = i + 1;
+	}
+	return 0;
 }
 
 DWORD WINAPI PingPongThreadProcNop(void *pingPongPtrs)
 {
-  volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
-  volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
+	volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
+	volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
 
-  *pPong = 0;
-  for (int i = 0; i < NITER; ++i)
-  {
-      while (*pPing < i)
-		  __nop();
-      *pPong = i+1;
-  }
-  return 0;
+	*pPong = 0;
+	for (int i = 0; i < NITER; ++i)
+	{
+		while (*pPing < i)
+			__nop();
+		*pPong = i + 1;
+	}
+	return 0;
 }
 
 DWORD WINAPI PingPongThreadProcPause(void *pingPongPtrs)
 {
-  volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
-  volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
+	volatile int *pPing = ((pingPongPtrs_t *)pingPongPtrs)->pPing;
+	volatile int *pPong = ((pingPongPtrs_t *)pingPongPtrs)->pPong;
 
-  *pPong = 0;
-  for (int i = 0; i < NITER; ++i)
-  {
-      while (*pPing < i)
-		  _mm_pause();
-      *pPong = i+1;
-  }
-  return 0;
+	*pPong = 0;
+	for (int i = 0; i < NITER; ++i)
+	{
+		while (*pPing < i)
+			_mm_pause();
+		*pPong = i + 1;
+	}
+	return 0;
 }
 
 int main(int argc, const char* argv[])
 {
-  DWORD (_stdcall *pPingPongThreadProc)(void *pingPongPtrs) = PingPongThreadProc;
-  if (argc > 1 && (strncmp(argv[1], "nop", 3) == 0))
-  {
-    pPingPongThreadProc = PingPongThreadProcNop;
-	printf("Executing NOP in the loop.\n");
-  }
+	DWORD(_stdcall *pPingPongThreadProc)(void *pingPongPtrs) = PingPongThreadProc;
+	if (argc > 1 && (strncmp(argv[1], "nop", 3) == 0))
+	{
+		pPingPongThreadProc = PingPongThreadProcNop;
+		printf("Executing NOP in the loop.\n");
+	}
 
-  if (argc > 1 && (strncmp(argv[1], "pause", 5) == 0))
-  {
-    pPingPongThreadProc = PingPongThreadProcPause;
-	printf("Executing PAUSE in the loop.\n");
-  }
+	if (argc > 1 && (strncmp(argv[1], "pause", 5) == 0))
+	{
+		pPingPongThreadProc = PingPongThreadProcPause;
+		printf("Executing PAUSE in the loop.\n");
+	}
 
-  SYSTEM_INFO si; GetSystemInfo(&si);
-  if (si.dwNumberOfProcessors > 1)
-  {
-      for (DWORD p1 = 0; p1 < si.dwNumberOfProcessors-1; ++p1)
-        for (DWORD p2 = p1+1; p2 < si.dwNumberOfProcessors; ++p2)
-        {
-            printf("CPU%d<->CPU%d:", p1, p2);
-            DWORD tid[2];
-            int ping[32]; // place ping and pong in different cache lines
-            int pong[32];
-            pingPongPtrs_t prm1 = {ping, pong};
-            pingPongPtrs_t prm2 = {pong, ping};
-            HANDLE h1 = CreateThread(NULL, 0, *pPingPongThreadProc,
-              &prm1, CREATE_SUSPENDED, &tid[0]);
-            HANDLE h2 = CreateThread(NULL, 0, *pPingPongThreadProc,
-              &prm2, CREATE_SUSPENDED, &tid[1]);
-            SetThreadAffinityMask(h1, 1 << p1);
-            SetThreadAffinityMask(h2, 1 << p2);
-            ping[0] = pong[0] = 0; // start in known state
-            ResumeThread(h1);
-            Sleep(10);  // Improve consistency of the time measurement
-            LARGE_INTEGER t0; QueryPerformanceCounter(&t0);
-            ResumeThread(h2);
-            WaitForSingleObject(h1, INFINITE);
-            LARGE_INTEGER t1; QueryPerformanceCounter(&t1);
-            // Cleanup
-            WaitForSingleObject(h2, INFINITE);
-            CloseHandle(h1);
-            CloseHandle(h2);
-            // Report result
-            __int64 dt = t1.QuadPart - t0.QuadPart;
-            LARGE_INTEGER fr; QueryPerformanceFrequency(&fr);
-            double tmNs = ((dt * 1E9)/(fr.QuadPart))/NITER;
-            printf(" %10.1fnS per ping-pong\n", tmNs);
-        }
-  }
-  else
-  {
-      printf("The test can't run on single-CPU machine. Sorry.\n");
-  }
-  return 0;
+	SYSTEM_INFO si; GetSystemInfo(&si);
+	if (si.dwNumberOfProcessors > 1)
+	{
+		for (DWORD p1 = 0; p1 < si.dwNumberOfProcessors - 1; ++p1)
+			for (DWORD p2 = p1 + 1; p2 < si.dwNumberOfProcessors; ++p2)
+			{
+				printf("CPU%d<->CPU%d:", p1, p2);
+				DWORD tid[2];
+				int ping[32]; // place ping and pong in different cache lines
+				int pong[32];
+				pingPongPtrs_t prm1 = { ping, pong };
+				pingPongPtrs_t prm2 = { pong, ping };
+				HANDLE h1 = CreateThread(NULL, 0, *pPingPongThreadProc,
+					&prm1, CREATE_SUSPENDED, &tid[0]);
+				HANDLE h2 = CreateThread(NULL, 0, *pPingPongThreadProc,
+					&prm2, CREATE_SUSPENDED, &tid[1]);
+				SetThreadAffinityMask(h1, 1 << p1);
+				SetThreadAffinityMask(h2, 1 << p2);
+				ping[0] = pong[0] = 0; // start in known state
+				ResumeThread(h1);
+				Sleep(10);  // Improve consistency of the time measurement
+				LARGE_INTEGER t0; QueryPerformanceCounter(&t0);
+				ResumeThread(h2);
+				WaitForSingleObject(h1, INFINITE);
+				LARGE_INTEGER t1; QueryPerformanceCounter(&t1);
+				// Cleanup
+				WaitForSingleObject(h2, INFINITE);
+				CloseHandle(h1);
+				CloseHandle(h2);
+				// Report result
+				__int64 dt = t1.QuadPart - t0.QuadPart;
+				LARGE_INTEGER fr; QueryPerformanceFrequency(&fr);
+				double tmNs = ((dt * 1E9) / (fr.QuadPart)) / NITER;
+				printf(" %10.1fnS per ping-pong\n", tmNs);
+			}
+	}
+	else
+	{
+		printf("The test can't run on single-CPU machine. Sorry.\n");
+	}
+	return 0;
 }
 //-------------------- The end
